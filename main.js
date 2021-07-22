d568e5436a59ee7c6bc7328d08bdab7189d91a01
Andrey <andreypodolskiy@bk.ru>
2021-07-22 22:56:27 +0300

    добавил логику кнопке Играть(работает не правильно) и комментарии к коду

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 let balance = 0;
 let bet = 0;
 const arrColor = ["Red", "Black", "Green", "Blue", "Orange"]
-const balanceInput = document.getElementById('balance');
-const refillInput = document.getElementById('refill');
-const enteredBet = document.getElementById('userBet');
-const selectColor = document.getElementsByName('color')
+const balanceInput = document.getElementById('balance'); //строка актуального баланса
+const refillInput = document.getElementById('refill'); //строка ввода количества денег
+const enteredBet = document.getElementById('userBet');//строка ввода ставки
+const selectColor = document.getElementsByName('color')//строка выбора цвета
 
-function addButtonClick() {
+function addButtonClick() { //пополняем баланс
     balance += Number(refillInput.value)
     refillInput.value = ""
     updateUI()
 }
 
-function runRoulette() {
-    if(randomElement() === userSelectColor()){
-        return userBetWinner()
-    } return userBetLose()
+function runRoulette() { //сравниваем предполагаемый и выигрышный цвет
+    winnerColor = randomElement()
+    selectedColor = userSelectColor()
+    document.getElementById('winner').innerHTML = winnerColor
+    return (winnerColor === selectColor) ? userBetWinner() : userBetLose()
 }
 
-function userBetLose() {
+function userBetLose() { //поведение при проигрыше
     balance -= Number(enteredBet.value)
     enteredBet.value = ""
     updateUI()
 }
 
-function userBetWinner() {
+function userBetWinner() { //поведение при выигрыше
     balance += Number(enteredBet.value * 2)
     enteredBet.value = ""
     updateUI()    
 }
 
-function randomElement() {
+function randomElement() { //возвращает выигрышный цвет
     let rand = Math.floor(Math.random() * arrColor.length)
     return arrColor[rand]
 }
 
-function userSelectColor() {
+function userSelectColor() {  //узнаём какой цвет выбрал игрок
     for(let i = 0; i < selectColor.length; i++) {
         if(selectColor[i].checked){
             return arrColor[i]
@@ -43,7 +44,8 @@ function userSelectColor() {
     }
 }
 
-function updateUI() {
+
+function updateUI() { //обновляет значение текущего баланса 
     balanceInput.value = balance
     document.getElementById('balance').innerHTML = balance
 }
